0d2b90c2c74775ab6b770fc10c1e9fbc204c726a
Rémy Coutable <rymai@rymai.com>
2010-09-09 17:29:31 +0200

    Removed useless :edit action in routes generation since we declare it in the block after

--- lib/devise_invitable/routes.rb
+++ lib/devise_invitable/routes.rb
@@ -5,7 +5,7 @@ module ActionDispatch
     protected
       
       def devise_invitation(mapping, controllers)
-        resource :invitation, :only => [:new, :create, :edit, :update],
+        resource :invitation, :only => [:new, :create, :update],
           :path => mapping.path_names[:invitation], :controller => controllers[:invitations] do
           get :edit, :path => mapping.path_names[:accept], :as => :accept
         end
